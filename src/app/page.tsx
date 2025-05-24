6b3c9365c537448d8a5216ad0016587c1db3621c
kevinhmz <79659794+kevinhmz@users.noreply.github.com>
2025-05-24 10:50:58 -0700

    link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
 "use client";
-// import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 import promo from "../../public/assets/promos/promo_24_mayo.png";
 import Image from "next/image";
 
 export default function Home() {
-  // const VideoPlayer = dynamic(() => import("../components/video/video"), {
-  //   ssr: false,
-  // });
+  const VideoPlayer = dynamic(() => import("../components/video/video"), {
+    ssr: false,
+  });
   return (
     <div className="home-container">
       <h2>Bif 5: El evento empieza el día 24 de mayo a las 6pm</h2>
-      {/* <VideoPlayer
-        url="https://servidorcadefi.inowu.dev/live/hls/bif.m3u8"
+      <VideoPlayer
+        url="https://live.inowu.dev/live/livestream-trans.m3u8"
         width="100%"
         loop={false}
         muted={false}
         controls={true}
-      /> */}
-      <div className="relative w-full h-full rounded-xl bg-neutral-500 overflow-hidden">
+      />
+      {/* <div className="relative w-full h-full rounded-xl bg-neutral-500 overflow-hidden">
         <Image src={promo} alt="" className="object-cover" fill />
-      </div>
+      </div> */}
     </div>
   );
 }
